e23ef0797a94e5a3afba1e22d20d8456b3004833
jhar <jhar87@gmail.com>
2016-10-28 09:06:31 -0700

    Initial commit (nothing works)

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	entry: ['babel-polyfill', 'isomorphic-fetch', './src/index.js'],
+	output: {
+		path: __dirname + '/dist/',
+		publicPath: 'dist/',
+		filename: 'build.js'
+	},
+	module: {
+		loaders: [
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
+        ]
+	},
+	babel: {
+		presets: ['es2015', 'stage-2'],
+		plugins: ['transform-runtime']
+	}
+};
