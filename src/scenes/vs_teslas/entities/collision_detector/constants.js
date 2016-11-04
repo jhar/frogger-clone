9979ae8b304cd461fad0d455f6f1f576761afc99
jhar <jhar87@gmail.com>
2016-11-04 08:08:46 -0700

    Fixes collision detection values

--- src/scenes/vs_teslas/entities/collision_detector/constants.js
+++ src/scenes/vs_teslas/entities/collision_detector/constants.js
@@ -5,6 +5,6 @@ import { FROG } from '../../../../entities/frog/constants'
 export function detectCollision(tesla) {
     if (FROG.dead === true) return 0
     if (FROG.y !== tesla.y) return false
-    let proximity = Math.abs(FROG.x - tesla.x + (TESLA_WIDTH / 2))
+    let proximity = Math.abs(FROG.x - (tesla.x + (TESLA_WIDTH / 2)))
     if (proximity < (TESLA_WIDTH / 2 * TESLA_FORGIVENESS)) return true
 }
